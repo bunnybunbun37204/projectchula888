12c70f2b552bc99b1b4b8d2f280ea5e843aff446
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-10 09:22:52 +0700

    feat: status color

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 // const URL = 'https://projectchula888.pages.dev'; // Assuming this is your base URL
 
 const fetchUser = async (ticket: string) => {
   console.log(ticket);
-  const response = await fetch(
-    `https://projectchula888-api.bunyawatapp37204.workers.dev/auth/register/${ticket}`,
-  );
+  const response = await fetch(`https://projectchula888-api.bunyawatapp37204.workers.dev/auth/register/${ticket}`);
   return response.json();
 };
 
@@ -15,6 +14,7 @@ function Callback() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [, setCookie] = useCookies(['student_id', 'student_lname', 'student_fname', 'student_email', 'student_faculty']);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -31,13 +31,14 @@ function Callback() {
       .then((userData) => {
         setUser(userData);
         setLoading(false);
-        console.log(userData);
+        setCookie('student_id', userData.result.id);
+        window.location.href = '/explore'
       })
       .catch(() => {
         setError(true);
         setLoading(false);
       });
-  }, []);
+  }, [setCookie]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
