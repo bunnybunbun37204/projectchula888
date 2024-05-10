7f5ffd5adc53c60a39bc2987c1366dc1fba1591c
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-10 11:02:34 +0700

    feat: status color

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -23,14 +23,7 @@ export default function Blog({ searchQuery }: BlogProps) {
     return (
       <div className="py-24 sm:py-32 ">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Explore</h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-              Learn how to grow your project with our expert advice.
-            </p>
-          </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {filteredBlogs.map((projects) => (
+           {filteredBlogs.map((projects) => (
               <article
                 key={projects.project_id}
                 className="flex max-w-xl flex-col justify-between transition-transform transform hover:scale-105 shadow-sm
@@ -76,7 +69,6 @@ export default function Blog({ searchQuery }: BlogProps) {
             ))}
           </div>
         </div>
-      </div>
     );
   }
 }
