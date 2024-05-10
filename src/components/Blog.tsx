617e664ad66cc0b39b7088d7216277fecf92f206
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-10 11:19:29 +0700

    feat: status color

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -21,7 +21,7 @@ export default function Blog({ searchQuery }: BlogProps) {
   if (blog) {
     const filteredBlogs = blog.filter((project) => project.title.toLowerCase().includes(searchQuery.toLowerCase()));
     return (
-      <div className="py-24 sm:py-32 ">
+      <div className="">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {filteredBlogs.map((projects) => (
