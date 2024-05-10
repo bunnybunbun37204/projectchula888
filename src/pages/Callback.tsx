8f2ae2f53531c249c06c1e1463dcf25a5ec35a3e
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-05-10 09:04:26 +0700

    feat: status color

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -29,7 +29,7 @@ function Callback() {
       .then((userData) => {
         setUser(userData);
         setLoading(false);
-        console.log(JSON.stringify(userData));
+        console.log(userData);
       })
       .catch(() => {
         setError(true);
